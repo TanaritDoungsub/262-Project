79c5e001c634de2a126926bc97bba47ce7da0e8a
TanaritDoungsub <tanarit.doun@bumail.net>
2024-11-12 00:00:32 +0700

    Start

---
+++ Assets/งาน/Scripts/Enemy.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour , IDamageable
+{
+    Transform targetDestination;
+    GameObject targetGameobject;
+    Character targetCharacter;
+    [SerializeField] float speed;
+
+    Rigidbody2D rgbd2d;
+
+    [SerializeField] int hp = 999;
+    [SerializeField] int damage = 1;
+
+    private void Awake()
+    {
+        rgbd2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        targetGameobject = target;
+        targetDestination = target.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = (targetDestination.position - transform.position).normalized;
+        rgbd2d.velocity = direction * speed;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject == targetGameobject)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        if (targetCharacter == null)
+        {
+            targetCharacter = targetGameobject.GetComponent<Character>();
+        }
+
+        targetCharacter.TakeDamage(damage);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+
+        if (hp < 1) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/งาน/Scripts/PlayerMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rgbd2D;
+    [HideInInspector]
+    public Vector3 movementVector;
+    [HideInInspector]
+    public float lastHorizontalVector;
+    [HideInInspector]
+    public float lastVerticalVector;
+
+    [SerializeField] float speed = 3f;
+
+    Animate animate;
+
+    private void Awake()
+    {
+        rgbd2D = GetComponent<Rigidbody2D>();
+        movementVector = new Vector3();
+        animate = GetComponent<Animate>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+
+        if (movementVector.x != 0)
+        {
+            lastHorizontalVector = movementVector.x;
+        }
+        if (movementVector.y != 0)
+        {
+            lastHorizontalVector = movementVector.y;
+        }
+
+        animate.horizontal = movementVector.x;
+
+        movementVector *= speed;
+
+        rgbd2D.velocity = movementVector;
+    }
+}
