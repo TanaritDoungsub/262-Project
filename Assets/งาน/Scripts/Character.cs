5a49a4d4fabe85cdca8f247bf95448b2fdd9b094
TanaritDoungsub <tanarit.doun@bumail.net>
2024-11-18 02:38:09 +0700

    Armor

--- Assets/งาน/Scripts/Character.cs
+++ Assets/งาน/Scripts/Character.cs
@@ -1,11 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
     public int maxHp = 1000;
     public int currentHp = 1000;
+
+    public int armor = 0;
+
     [SerializeField] StatusBar hpBar;
 
     [HideInInspector] public Level level;
@@ -24,6 +29,9 @@ public class Character : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+
+        ApplyArmor(ref damage);
+
         currentHp -= damage;
 
         if (currentHp <= 0)
@@ -32,6 +40,16 @@ public class Character : MonoBehaviour
         }
         hpBar.SetState(currentHp, maxHp);
     }
+
+    private void ApplyArmor(ref int damage)
+    {
+        damage -= armor;
+        if (damage < 0)
+        {
+            damage = 0;
+        }
+    }
+
     public void Heal(int amount)
     {
         if (currentHp <= 0) { return; }
--- Assets/งาน/Scripts/PlayerMove.cs
+++ Assets/งาน/Scripts/PlayerMove.cs
@@ -24,6 +24,12 @@ public class PlayerMove : MonoBehaviour
         animate = GetComponent<Animate>();
     }
 
+    private void Start()
+    {
+        lastHorizontalVector = -1f;
+        lastVerticalVector = 1f;
+    }
+
     // Update is called once per frame
     void Update()
     {
