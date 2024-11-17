bf1ee82bec69dd73d3a54c354def551a1803cc26
TanaritDoungsub <tanarit.doun@bumail.net>
2024-11-18 01:54:45 +0700

    Code Knife Peojectile

--- Assets/ThrowingDaggerKnife.cs
+++ Assets/ThrowingDaggerKnife.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class ThrowingDaggerKnife : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float timeToAttack;
+    float timer;
+
+    PlayerMove playerMove;
+
+    [SerializeField] GameObject knifePerfab;
+
+    private void Awake()
     {
-        
+        playerMove = GetComponentInParent<PlayerMove>();
+    }
+    private void Update()
+    {
+        if (timer < timeToAttack)
+        {
+            timer += Time.deltaTime;
+            return;
+        }
+
+        timer = 0;
+        SpawnKnife();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnKnife()
     {
-        
+        GameObject thrownKnife = Instantiate(knifePerfab);
+        thrownKnife.transform.position = transform.position;
+        thrownKnife.GetComponent<ThrowingDaggerProjectile>().SetDirection(playerMove.lastHorizontalVector, 0f);
     }
 }
